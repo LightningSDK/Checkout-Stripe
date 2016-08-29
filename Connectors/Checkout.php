51419ba555311c9dcf800c708e28fc11c529bc57
DAB <dab@dab.la>
2016-08-29 15:55:37 -0500

    added checkout connector for initializing payment handler

---
+++ Connectors/Checkout.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Stripe\Connectors;
+
+use Lightning\Tools\Configuration;
+use Lightning\View\JS;
+
+class Checkout {
+    public static function init() {
+        JS::startup('lightning.modules.stripe.init();', ['/js/checkout.min.js']);
+        JS::set('modules.stripe.public', Configuration::get('stripe.public'));
+        JS::set('modules.checkout.handler', 'lightning.modules.stripe.pay');
+    }
+}
